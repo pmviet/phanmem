1c430eeabae58f53e73123952775dee44014b667
PM Viet <hotro.gocsv@gmail.com>
2025-09-08 18:55:28 +0700

    Update date.js
    Cập nhật từ tháng/ngày/năm thành ngày/tháng/năm

--- js/date.js
+++ js/date.js
@@ -1,48 +1,50 @@
 $(document).ready(function() {
-    $('#reportrange').daterangepicker(
-       {
-          startDate: moment().subtract('month', 1).add(1, 'days'),
-          endDate: moment(),
-          minDate: '01/01/2020 0:0:0',
-          maxDate: moment().endOf('month'),
-          showDropdowns: true,
-          showWeekNumbers: true,
-          timePicker: false,
-          timePickerIncrement: 1,
-          timePicker12Hour: true,
-          ranges: {
-             'Hôm nay': [moment(), moment()],
-             'Hôm qua': [moment().subtract('days', 1), moment().subtract('days', 1)],
-             '7 ngày gần đây': [moment().subtract('days', 6), moment()],
-             '1 tháng gần đây': [moment().subtract('month', 1).add(1, 'days'), moment()],
-             'Tháng này': [moment().startOf('month'), moment().endOf('month')],
-             'Tháng trước': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')],
-             'Năm nay': [moment().startOf('year'), moment().endOf('year')],
-             'Năm trước': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
-             '2 năm qua': [moment().subtract(2, 'years').startOf('year'), moment().endOf('year')],
-          },
-          opens: 'left',
-          buttonClasses: ['btn btn-default'],
-          applyClass: 'btn-small btn-primary',
-          cancelClass: 'btn-small',
-          format: 'DD/MM/YYYY',
-          separator: ' to ',
-          locale: {
-              applyLabel: 'Xem',
-              fromLabel: 'From',
-              toLabel: 'To',
-              customRangeLabel: 'Tùy chỉnh',
-              daysOfWeek: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6','T7'],
-              monthNames: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
-              firstDay: 1
-          }
-       },
-       function(start, end) {
-        console.log("Callback has been called!");
-        $('#reportrange span').html(start.format('D MMMM YYYY') + ' - ' + end.format('D MMMM YYYY'));
+  $('#reportrange').daterangepicker(
+    {
+      startDate: moment().subtract(1, 'month').add(1, 'days'),
+      endDate: moment(),
+      minDate: '01/01/2020',
+      maxDate: moment().endOf('month'),
+      showDropdowns: true,
+      showWeekNumbers: true,
+      timePicker: false,
+      timePickerIncrement: 1,
+      timePicker12Hour: true,
+      opens: 'left',
+      buttonClasses: ['btn btn-default'],
+      applyClass: 'btn-small btn-primary',
+      cancelClass: 'btn-small',
+      locale: {
+        format: 'DD/MM/YYYY', // ✅ Đặt đúng vị trí
+        separator: ' đến ',
+        applyLabel: 'Xem',
+        cancelLabel: 'Hủy',
+        fromLabel: 'Từ',
+        toLabel: 'Đến',
+        customRangeLabel: 'Tùy chỉnh',
+        daysOfWeek: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6','T7'],
+        monthNames: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
+        firstDay: 1
+      },
+      ranges: {
+        'Hôm nay': [moment(), moment()],
+        'Hôm qua': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        '7 ngày gần đây': [moment().subtract(6, 'days'), moment()],
+        '1 tháng gần đây': [moment().subtract(1, 'month').add(1, 'days'), moment()],
+        'Tháng này': [moment().startOf('month'), moment().endOf('month')],
+        'Tháng trước': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+        'Năm nay': [moment().startOf('year'), moment().endOf('year')],
+        'Năm trước': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
+        '2 năm qua': [moment().subtract(2, 'years').startOf('year'), moment().endOf('year')]
+      }
+    },
+    function(start, end) {
+      $('#reportrange span').html(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
+    }
+  );
 
-       }
-    );
-    //Set the initial state of the picker label
-    $('#reportrange span').html(moment().subtract('days', 29).format('D MMMM YYYY') + ' - ' + moment().format('D MMMM YYYY'));
- });
+  // Gán giá trị mặc định ban đầu
+  $('#reportrange span').html(
+    moment().subtract(29, 'days').format('DD/MM/YYYY') + ' - ' + moment().format('DD/MM/YYYY')
+  );
+});
